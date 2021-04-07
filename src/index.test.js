e50e9f8ff8815f58d1e335562462b327e47ce692
Mateusz Choma <kontakt@coderoad.pl>
2021-04-07 17:26:20 +0200

    False positive async tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,41 @@
 it('should pass', () => {
     expect(true).toBe(true)
 })
+
+it('should pass with setTimeout', () => {
+    setTimeout(
+        () => expect(true).toBe(true),
+        1000
+    )
+})
+
+it('should fail with setTimeout', () => {
+    setTimeout(
+        () => expect(true).toBe(false),
+        1000
+    )
+})
+
+it('should pass with resolved promise', () => {
+    Promise.resolve('some data').then((data) => {
+        expect(data).toBe('some data')
+    })
+})
+
+it('should fail with rejected promise', () => {
+    Promise.reject('some data').then((data) => {
+        expect(data).toBe('some data')
+    })
+})
+
+it('should fail with resolved promise', () => {
+    Promise.resolve('some data').catch((error) => {
+        expect(error).toBe('some data')
+    })
+})
+
+it('should pass with rejected promise', () => {
+    Promise.reject('some data').catch((error) => {
+        expect(error).toBe('some data')
+    })
+})
