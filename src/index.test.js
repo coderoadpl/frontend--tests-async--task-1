3ebbcdb8708d8d695091601fc1d0d3a5b69354dd
Mateusz Choma <kontakt@coderoad.pl>
2021-04-07 17:31:56 +0200

    `expect.assertions`

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,10 @@
 it('should pass', () => {
+    expect.assertions(1)
     expect(true).toBe(true)
 })
 
 it('should pass with setTimeout', () => {
+    expect.assertions(1)
     setTimeout(
         () => expect(true).toBe(true),
         1000
@@ -10,6 +12,7 @@ it('should pass with setTimeout', () => {
 })
 
 it('should fail with setTimeout', () => {
+    expect.assertions(1)
     setTimeout(
         () => expect(true).toBe(false),
         1000
@@ -17,24 +20,28 @@ it('should fail with setTimeout', () => {
 })
 
 it('should pass with resolved promise', () => {
+    expect.assertions(1)
     Promise.resolve('some data').then((data) => {
         expect(data).toBe('some data')
     })
 })
 
 it('should fail with rejected promise', () => {
+    expect.assertions(1)
     Promise.reject('some data').then((data) => {
         expect(data).toBe('some data')
     })
 })
 
 it('should fail with resolved promise', () => {
+    expect.assertions(1)
     Promise.resolve('some data').catch((error) => {
         expect(error).toBe('some data')
     })
 })
 
 it('should pass with rejected promise', () => {
+    expect.assertions(1)
     Promise.reject('some data').catch((error) => {
         expect(error).toBe('some data')
     })
